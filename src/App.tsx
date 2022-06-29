b58ccafa15879a80ae0c3dcba56373e0949c9a77
Ciahaka <wilddiver.by@gmail.com>
2022-06-29 22:46:04 +0300

    add Todo_2/1_Victor

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TasksTodolist, Todolist} from "./Todolist";
 
 function App() {
-
   const title_1: string = 'Чо учить??'
   const title_2: string = 'Чо купить??'
-  const title_3: string = 'Где тусить??'
+  const title: string = 'Где тусить??'
 
-  const tasks_1: Array<TasksTodolist> = [
-    {id: 1, isDone: true, title: 'React'},
-    {id: 2, isDone: true, title: 'CSS'},
-    {id: 3, isDone: false, title: 'ES6'},
+  // const tasks_1: Array<TasksTodolist> = [
+  //     {id: 1, isDone: true, title: 'React'},
+  //     {id: 2, isDone: true, title: 'CSS'},
+  //     // {id: 3, isDone: false, title: 'ES6'},
 
-  ]
-  const tasks_2: Array<TasksTodolist> = [
-    {id: 1, isDone: false, title: 'Bread'},
-    {id: 2, isDone: true, title: 'Butter'},
-    {id: 3, isDone: true, title: 'Milk'},
+  // ]
+  // const tasks_2: Array<TasksTodolist> = [
+  //     {id: 1, isDone: false, title: 'Bread'},
+  //     {id: 2, isDone: true, title: 'Butter'},
+  //     {id: 3, isDone: true, title: 'Milk'},
+  //
+  // ]
 
-  ]
+  const [tasks,setTasks] =useState(
+    [
+      {id: 1, isDone: false, title: 'Club'},
+      {id: 2, isDone: true, title: 'Museum'},
+      {id: 3, isDone: false, title: 'Street'},
+      {id: 4, isDone: true, title: 'Gym'},
+    ]
+  )
 
-  const tasks_3: Array<TasksTodolist> = [
-    {id: 1, isDone: false, title: 'Club'},
-    {id: 2, isDone: false, title: 'Museum'},
-    {id: 3, isDone: true, title: 'Street'},
+  // let tasks: Array<TasksTodolist> = [
+  //   {id: 1, isDone: false, title: 'Club'},
+  //   {id: 2, isDone: true, title: 'Museum'},
+  //   {id: 3, isDone: false, title: 'Street'},
+  //   {id: 4, isDone: true, title: 'Gym'},
+  // ]
 
-  ]
+  const deleteTask = (taskID:number) => {
+    // alert('Upsss')
+   // const updateTasks = tasks.filter(task=>task.id!==taskID)
+   //  setTasks(taskID)
+    setTasks(tasks.filter(task=>task.id!==taskID))
+  }
 
   return (
 
     <div className="App">
-      <Todolist title={title_1} tasks={tasks_1}/>
-      <Todolist title={title_2} tasks={tasks_2}/>
-      <Todolist title={title_3} tasks={tasks_3}/>
+      {/*<Todolist title={title_1} tasks={tasks_1}/>*/}
+      {/*<Todolist title={title_2} tasks={tasks_2}/>*/}
+      <Todolist
+        title={title}
+        tasks={tasks}
+        deleteTask={deleteTask}
+      />
 
     </div>
   );
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,43 +1,43 @@
 import React from 'react';
 
-export type TasksTodolist={
+export type TasksTodolist = {
   id: number
-  title:string
+  title: string
   isDone: boolean
 }
 
-type  TodolistType ={
+type TodolistType = {
   title: string
   tasks: TasksTodolist[]
+  deleteTask: (taskID:number) => void
+
 }
 
-export const Todolist = (props:TodolistType) => {
-  return (
-    <div>
-      <div>
-        <h3>{props.title}</h3>
-        <div>
-          <input/>
-          <button>+</button>
-        </div>
-        <ul>
-          <li>
-            <input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span>
-          </li>
-          <li>
-            <input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span>
-          </li>
-          <li>
-            <input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span>
-          </li>
-        </ul>
-        <div>
-          <button>All</button>
-          <button>Active</button>
-          <button>Completed</button>
-        </div>
-      </div>
-    </div>
-  );
-};
+export const Todolist = (props: TodolistType) => {
+  const tasksListItem = props.tasks.map(task => {
+    const deleteTask =()=> props.deleteTask(task.id)
+    return (
+      <li>
+        <input type="checkbox" checked={task.isDone}/>
+        <span>{task.title}</span>
+        <button onClick= {deleteTask}>x</button>
+      </li>
 
+    )
+  })
+  return <div>
+    <h3>{props.title}</h3>
+    <div>
+      <input/>
+      <button>+</button>
+    </div>
+    <ul>
+      {tasksListItem}
+    </ul>
+    <div>
+      <button>All</button>
+      <button>Active</button>
+      <button>Completed</button>
+    </div>
+  </div>
+}
